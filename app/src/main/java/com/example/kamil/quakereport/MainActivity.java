33a1c51c9e0b9bbd36bc80745acb55113eb968b6
GlaDdos <kamil.kawa@gmail.com>
2018-06-30 22:46:44 +0200

    Basic list view

--- app/src/main/java/com/example/kamil/quakereport/MainActivity.java
+++ app/src/main/java/com/example/kamil/quakereport/MainActivity.java
@@ -2,12 +2,35 @@ package com.example.kamil.quakereport;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String LOG_TAG = MainActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+            // Create a fake list of earthquake locations.
+            ArrayList<String> earthquakes = new ArrayList<>();
+            earthquakes.add("San Francisco");
+            earthquakes.add("London");
+            earthquakes.add("Tokyo");
+            earthquakes.add("Mexico City");
+            earthquakes.add("Moscow");
+            earthquakes.add("Rio de Janeiro");
+            earthquakes.add("Paris");
+
+            ListView earthquakeListView = (ListView) findViewById(R.id.list);
+
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                    this, android.R.layout.simple_list_item_1, earthquakes);
+
+            earthquakeListView.setAdapter(adapter);
+        }
 }

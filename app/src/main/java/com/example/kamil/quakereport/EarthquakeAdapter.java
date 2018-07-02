7e998b8f2d21c2abdb05e3081af60c02b8e64d40
GlaDdos <kamil.kawa@gmail.com>
2018-07-02 17:57:52 +0200

    Visual polish. splitting location string, and date/time string

--- app/src/main/java/com/example/kamil/quakereport/EarthquakeAdapter.java
+++ app/src/main/java/com/example/kamil/quakereport/EarthquakeAdapter.java
@@ -1,8 +1,11 @@
 package com.example.kamil.quakereport;
 
 import android.app.Activity;
+import android.graphics.drawable.GradientDrawable;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -31,15 +34,72 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
 
         Earthquake quake = getItem(position);
 
+        TextView tvTime = (TextView) listItemView.findViewById(R.id.tvTime);
+        TextView tvDate = (TextView) listItemView.findViewById(R.id.tvDate);
+        String[] dateTime = quake.getDateString().split(" ");
+
+        tvDate.setText(dateTime[0]);
+        tvTime.setText(dateTime[1]);
+
+
+        //splitting location string to primary location and offset
+        String[] locationArray = quake.getLocation().split(" of ");
+
+        TextView tvLocationOffset = (TextView) listItemView.findViewById(R.id.tvOffsetLocation);
+        TextView tvLocation = (TextView) listItemView.findViewById(R.id.tvLocation);
+
+        if (locationArray.length > 1) {
+            tvLocationOffset.setText(locationArray[0] + " of");
+            tvLocation.setText(locationArray[1]);
+        } else {
+            tvLocationOffset.setText("Near the ");
+            tvLocation.setText(locationArray[0]);
+        }
+
         TextView tvMagnitude = (TextView) listItemView.findViewById(R.id.tvMagnitude);
         tvMagnitude.setText(String.valueOf(quake.getMagnitude()));
 
-        TextView tvLocation = (TextView) listItemView.findViewById(R.id.tvLocation);
-        tvLocation.setText(quake.getLocation());
-
-        TextView tvDate = (TextView) listItemView.findViewById(R.id.tvDate);
-        tvDate.setText(quake.getDateString());
+        GradientDrawable magnitudeCircle = (GradientDrawable) tvMagnitude.getBackground();
+        int magnitudeColor = getMagnitudeColor(quake.getMagnitude());
+        magnitudeCircle.setColor(magnitudeColor);
 
         return listItemView;
+    }
+
+    private int getMagnitudeColor(double magnitude) {
+
+        switch ((int) Math.ceil(magnitude)) {
+            case 0:
+            case 1:
+            case 2:
+                return ContextCompat.getColor(getContext(), R.color.magnitude1);
+
+            case 3:
+                return ContextCompat.getColor(getContext(), R.color.magnitude2);
+
+            case 4:
+                return ContextCompat.getColor(getContext(), R.color.magnitude3);
+
+            case 5:
+                return ContextCompat.getColor(getContext(), R.color.magnitude4);
+
+            case 6:
+                return ContextCompat.getColor(getContext(), R.color.magnitude5);
+
+            case 7:
+                return ContextCompat.getColor(getContext(), R.color.magnitude6);
+
+            case 8:
+                return ContextCompat.getColor(getContext(), R.color.magnitude7);
+
+            case 9:
+                return ContextCompat.getColor(getContext(), R.color.magnitude8);
+
+            case 10:
+                return ContextCompat.getColor(getContext(), R.color.magnitude9);
+
+            default:
+                return ContextCompat.getColor(getContext(), R.color.magnitude10plus);
+        }
     }
 }
